35529d756ac8f9f978849ee055418bcaa40c0322
Andrew Memme <andrew.memme4@gmail.com>
2021-08-13 10:20:27 -0400

    updating comments

--- MechaCarAnalysis.R
+++ MechaCarAnalysis.R
@@ -1,7 +1,7 @@
 # Import dplyr library
 library(dplyr)
 
-# Deliverable 1 - Linear Regression to Predict MPG
+# Linear Regression to Predict MPG
 # Import and read csv file into dataframe
 df<- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 
@@ -12,19 +12,20 @@ multiple_regress <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + g
 summary(multiple_regress)
 
 
-# Deliverable 2 - Summary Stats on Suspension Coils  
+#Summary Stats on Suspension Coils  
 # Import and read csv file into dataframe
 suspension <-read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 
 # Summarize mean, median, variance, and std dev of PSI column in a new dataframe
 total_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI),
                                               Variance=var(PSI), SD=sd(PSI), .groups='keep')
-# Group by lot number and summarize mean, median, variance, and std dev of PSI column in a new df
+# Group by lot number
+# Summarize mean, median, variance, and std dev of PSI column in a new df
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),
                                                                         Variance=var(PSI), SD=sd(PSI), .groups='keep')
 
 
-# Deliverable 3 - T-test analysis 
+# T-test analysis 
 # Visualize distribution
 plt <- ggplot(suspension, aes(x=PSI))
 plt + geom_density()
@@ -36,6 +37,7 @@ t.test(suspension$PSI,mu=1500)
 lot_1 <- subset(suspension, Manufacturing_Lot=='Lot1')
 lot_2 <- subset(suspension, Manufacturing_Lot=='Lot2')
 lot_3 <- subset(suspension, Manufacturing_Lot=='Lot3')
+
 #Visualize distributions 
 #Lot1
 plt <- ggplot(lot_1, aes(x=PSI))
